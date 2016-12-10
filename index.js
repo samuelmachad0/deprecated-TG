aa51a2de4e99d53566663a722568a1cfc4fd0bfc
Samuel Machado <samuel.codigo@gmail.com>
2016-12-10 10:36:17 -0200

    Fixed the http server

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 "use strict";
+var express = require('express');
+var app = express();
 
-var http = require("http");
-var url = require('url');
-var port = (process.env.PORT || 5000);
+app.get('/', function (req, res) {
+  var TelegramBot = require('node-telegram-bot-api');
 
-
-http.createServer(function(request, response){
-var TelegramBot = require('node-telegram-bot-api');
-
-  response.writeHead(response.statusCode.toString(),{'Content-Type':'application/json'});
-  response.write(JSON.stringify({name: 'smartcitiesbot', ver:'0.0.1'}));
     var token = '235548784:AAHkS-f8J4D4LTM527TldPFHRKt0DL1ykB4';
     var parts = url.parse(request.url, true);
     var query = parts.query;
   var bot = new TelegramBot(token, { polling: true });
   bot.sendMessage("153878723",  request.query.valor);
-  response.end();
-}).listen(port);
+  res.send("porra");
+});
 
-
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
