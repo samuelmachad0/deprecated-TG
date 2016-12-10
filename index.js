df8efcf9da51652125f51932c7eb2ec976d2b861
Samuel Machado <samuel.codigo@gmail.com>
2016-12-10 09:59:25 -0200

    Fixed the http server

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ require('./bot.js')();
 
 http.createServer(function(request, response){
 
-  response.writeHead(response.statusCode.toString(),{'Content-Type':'application/json','method': 'POST'});
+  response.writeHead(response.statusCode.toString(),{'Content-Type':'application/json'});
   response.write(JSON.stringify({name: 'smartcitiesbot', ver:'0.0.1'}));
-  response.end();
+      if(request.method == "GET")
+        {
+            response.end("received GET request.")
+        }
+    else if(request.method == "POST")
+        {
+            response.end("received POST request.");
+        }
 }).listen(port);
 
 
