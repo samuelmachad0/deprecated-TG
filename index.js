35ee5693534e35101e65e928cb7db167f4f2c3ef
Samuel Machado <samuel.codigo@gmail.com>
2016-12-10 10:34:14 -0200

    Fixed the http server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var http = require("http");
-
+var url = require('url');
 var port = (process.env.PORT || 5000);
 
 
@@ -11,8 +11,10 @@ var TelegramBot = require('node-telegram-bot-api');
   response.writeHead(response.statusCode.toString(),{'Content-Type':'application/json'});
   response.write(JSON.stringify({name: 'smartcitiesbot', ver:'0.0.1'}));
     var token = '235548784:AAHkS-f8J4D4LTM527TldPFHRKt0DL1ykB4';
+    var parts = url.parse(request.url, true);
+    var query = parts.query;
   var bot = new TelegramBot(token, { polling: true });
-  bot.sendMessage("153878723",  response.data);
+  bot.sendMessage("153878723",  request.query.valor);
   response.end();
 }).listen(port);
 
