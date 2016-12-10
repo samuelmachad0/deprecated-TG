f3cfbc9aba776dab30c0746f1f3d65b1d47249d2
Samuel Machado <samuel.codigo@gmail.com>
2016-12-10 10:29:20 -0200

    Fixed the http server

--- bot.js
+++ bot.js
@@ -1,21 +1,38 @@
 "use strict";
 var TelegramBot = require('node-telegram-bot-api');
-
 module.exports = function(){
   var token = '235548784:AAHkS-f8J4D4LTM527TldPFHRKt0DL1ykB4';
   var bot = new TelegramBot(token, { polling: true });
-  // bot.onText(/\/echo (.+)/, function (msg, match) {
-  //   var chatId = msg.chat.id;
-  //   var resp = match[1]; 
-  //   bot.sendMessage(chatId, resp);
-  // });
+
+  // msg = json do telegram
   bot.on('message', function (msg) {
     var chatId = msg.chat.id;
 
-    bot.sendMessage(chatId,  msg.text);
+    bot.sendMessage(chatId,  response.data);
    });
 }
-
+// json do telegram, 
+ // {
+ //      "update_id": 782684373,
+ //      "message": {
+ //        "message_id": 123,
+ //        "from": {
+ //          "id": 153878723,
+ //          "first_name": "Samuel",
+ //          "last_name": "Alves",
+ //          "username": "samuelmachado"
+ //        },
+ //        "chat": {
+ //          "id": 153878723,
+ //          "first_name": "Samuel",
+ //          "last_name": "Alves",
+ //          "username": "samuelmachado",
+ //          "type": "private"
+ //        },
+ //        "date": 1481370898,
+ //        "text": "sda"
+ //      }
+ //    }
 
 
 
