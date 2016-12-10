23ed57351f31da4b19f972125025662b02d825d8
Samuel Machado <samuel.codigo@gmail.com>
2016-12-10 10:14:35 -0200

    Fixed the http server

--- bot.js
+++ bot.js
@@ -4,15 +4,15 @@ var TelegramBot = require('node-telegram-bot-api');
 module.exports = function(){
   var token = '235548784:AAHkS-f8J4D4LTM527TldPFHRKt0DL1ykB4';
   var bot = new TelegramBot(token, { polling: true });
-  bot.onText(/\/echo (.+)/, function (msg, match) {
-    var chatId = msg.chat.id;
-    var resp = match[1]; 
-    bot.sendMessage(chatId, resp);
-  });
+  // bot.onText(/\/echo (.+)/, function (msg, match) {
+  //   var chatId = msg.chat.id;
+  //   var resp = match[1]; 
+  //   bot.sendMessage(chatId, resp);
+  // });
   bot.on('message', function (msg) {
     var chatId = msg.chat.id;
 
-    bot.sendMessage(chatId, "Suzana");
+    bot.sendMessage(chatId,  msg.text);
    });
 }
 
