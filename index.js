35b157e920565697d17e3539ff79746a49442c91
Samuel Machado <samuel.codigo@gmail.com>
2017-04-14 13:27:32 -0300

    test

--- index.js
+++ index.js
@@ -8,15 +8,37 @@ var port = (process.env.PORT || 5000);
 
 
 
-setInterval(function() {
-  http.get('https://nodejschatbot.herokuapp.com/');
-}, 1800000); 
+// setInterval(function() {
+//   http.get('https://nodejschatbot.herokuapp.com/');
+// }, 1800000); 
 
 var token = '235548784:AAHkS-f8J4D4LTM527TldPFHRKt0DL1ykB4';
 var bot = new TelegramBot(token, { polling: true });
 
   
 bot.on('message', function (msg) {
+  // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+
   var chatId = msg.chat.id;
 
   bot.sendMessage(chatId,  msg.text);
