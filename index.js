01b66ba208ddb5400e49e6b030461858579adf45
Samuel Machado <samuel.codigo@gmail.com>
2017-04-14 13:16:59 -0300

    test

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 "use strict";
 
 var http = require("http");
-
-var express = require('express');
-var app = express();
-
 var TelegramBot = require('node-telegram-bot-api');
 
 var port = (process.env.PORT || 5000);
@@ -13,42 +9,24 @@ var port = (process.env.PORT || 5000);
 
 
 setInterval(function() {
-  http.get('https://testnodebot.herokuapp.com/');
+  http.get('https://nodejschatbot.herokuapp.com/');
 }, 1800000); 
 
 var token = '235548784:AAHkS-f8J4D4LTM527TldPFHRKt0DL1ykB4';
 var bot = new TelegramBot(token, { polling: true });
 
   
-bot.onText(/ADS/, function (msg) {
-  
-
+bot.on('message', function (msg) {
   var chatId = msg.chat.id;
-  
-  bot.sendMessage(chatId, 'O curso de Análise e Desenvolvimento de Sistemas ...');
+
+  bot.sendMessage(chatId,  msg.text);
 });
 
-bot.onText(/BD/, function (msg) {
-  
 
-  var chatId = msg.chat.id;
-  
-  bot.sendMessage(chatId, 'O curso de Banco de Dados ...');
-});
 
-bot.onText(/\/cursos/, function (msg) {
-  var chatId = msg.chat.id;
-  var opts = {
-      reply_to_message_id: msg.message_id,
-      reply_markup: JSON.stringify({
-        keyboard: [
-          ['ADS'],
-          ['BD']]
-      })
-    };
-    bot.sendMessage(chatId, 'Sobre qual curso você deseja receber informações?', opts);
-});
+http.createServer(function(request, response){
 
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
-});
+  response.writeHead(response.statusCode.toString(),{'Content-Type':'application/json'});
+  response.write(JSON.stringify({name: 'smartcitiesbot', ver:'0.0.1'}));
+  response.end();
+}).listen(port);
