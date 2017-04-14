34560d669d239c8047dc0aa3ad1d7683512801be
Samuel Machado <samuel.codigo@gmail.com>
2017-04-14 13:34:50 -0300

    test

--- index.js
+++ index.js
@@ -1,49 +1,34 @@
-"use strict";
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-var http = require("http");
-var TelegramBot = require('node-telegram-bot-api');
+var CONTACTS_COLLECTION = "users";
 
-var port = (process.env.PORT || 5000);
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
-
-
-// setInterval(function() {
-//   http.get('https://nodejschatbot.herokuapp.com/');
-// }, 1800000); 
-
-var token = '235548784:AAHkS-f8J4D4LTM527TldPFHRKt0DL1ykB4';
-var bot = new TelegramBot(token, { polling: true });
-
-  
-bot.on('message', function (msg) {
-
-  var chatId = msg.chat.id;
-
-  bot.sendMessage(chatId,  msg.text);
-  var newContact = '{"a":"b"}';
-  db.collection(users).insertOne(newContact, function(err, doc) {
-   
-  });
-
-});
-
-
-
+// Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
-    // Save database object from the callback for reuse.
+
+  // Save database object from the callback for reuse.
   db = database;
   console.log("Database connection ready");
 
-http.createServer(function(request, response){
-
-  response.writeHead(response.statusCode.toString(),{'Content-Type':'application/json'});
-  response.write(JSON.stringify({name: 'smartcitiesbot', ver:'0.0.1'}));
-  response.end();
-}).listen(port);
-
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
+
+// CONTACTS API ROUTES BELOW
