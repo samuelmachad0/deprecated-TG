834c63422f94e11e324aac4d5cf24ba531b2cefa
Samuel Machado <samuel.codigo@gmail.com>
2017-04-17 20:00:37 -0300

    sadasd

--- index.js
+++ index.js
@@ -47,8 +47,7 @@ bot.on('message', function (msg) {
  }
  if (msg.text.match("✅ Ativar Notificações")) {
     var doc = {chat_id: chatId, name: msg.chat.first_name, type: "User"  };
-    db.collection('users').update({chat_id:chatId, doc, upsert:true});
-
+    db.collection('users').update({chat_id:chatId}, {doc}, {upsert:true});
     console.log("Incluir!");
  }
  if (msg.text.match("Verificar Leitura")) {
