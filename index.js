0f277b4a7ed86f5d96c395089dc9888f46e91596
Samuel Machado <samuel.codigo@gmail.com>
2017-04-18 19:09:01 -0300

    sadasd

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ bot.on('message', function (msg) {
 
 });
 
-function sendShit(response,chatId, message_id = 0){
+function sendShit(response,chatId, message_id = ''){
    db.collection('users').count({ _id: chatId }, function(err, countDocuments) {
     console.log(countDocuments);
     if(parseInt(countDocuments) > 0){
